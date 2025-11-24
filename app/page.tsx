dfbc89fcc41adb32717de423bc9174040effd85a
369webforall <369webforall@gmail.com>
2025-11-24 07:59:42 +0545

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+const Home = () => {
+  return (
+    <div className="font-game text-3xl">
+      Home <Button>click to add</Button>
+    </div>
+  );
+};
+
+export default Home;
